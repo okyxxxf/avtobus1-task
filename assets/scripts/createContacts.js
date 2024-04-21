c7fc8ee45e9313ef068aece4e9027e436a696258
alexander <okyxxxf@gmail.com>
2024-04-21 12:10:41 +0300

    feat: create functionality to create contact

---
+++ assets/scripts/createContacts.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+  const contactsModal = document.querySelector("#contactsModal");
+  const phoneInput = contactsModal.querySelector(".input-phone");
+  const nameInput = contactsModal.querySelector(".input-name");
+  const createButton = contactsModal.querySelector(".save-btn");
+  const selectGroup = contactsModal.querySelector(".group-select");
+
+  createButton.addEventListener("click", () => {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (!phoneInput.value || !nameInput.value || !selectGroup.value) return window.location.reload();
+
+    const newContact = {name: nameInput.value, number: phoneInput.value, group: selectGroup.value};
+    localStorage.setItem("contacts", JSON.stringify([...parsedContacts, newContact]));
+
+    window.location.reload();
+  });
+
+});
